d33ccceb56e25ed68f173f79ced3ebfdd2ccac99
RileyM117 <98477085+RileyM117@users.noreply.github.com>
2022-11-18 16:22:44 -0600

    Update action.py

--- action.py
+++ action.py
@@ -1,9 +1,13 @@
-# Simple command function probably needs wait param
+# Simple command function to communicate with Camelot. Likely needs wait parameter to delay certain actions.
 
 def action(command):
+    
       print('start ' + command)
-      while(True):
+      # Get responses until success or failure response is received from Camelot
+      while(True):  
+        # Get response from Camelot    
         i = input()
+         # Return True if succeeds and False if fails.
         if(i == 'succeeded ' + command):
           return True
         elif(i == 'failed ' + command):
