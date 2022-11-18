f29782a78760f8d1f95a651fb50a287bc60433ee
RileyM117 <98477085+RileyM117@users.noreply.github.com>
2022-11-18 16:26:50 -0600

    Update blacksmith_shop_scene.py

--- blacksmith_shop_scene.py
+++ blacksmith_shop_scene.py
@@ -1,11 +1,12 @@
 # Blacksmith shop scene
+# In this scene, The blacksmith goes to put a Sword in the chest in his shop
+# He then notices that his Gem is missing and goes to tell the others.
 
-#customize camera and sounds and time of day
 def blacksmith_scene():
   action('Putdown(Smithy,hammer)')
-  action('Pickup(Smithy,Sword)')
-  action('WalkTo(Smithy,Blacksmith.Chest)')
-  action('LookAt(Smithy,Blacksmith.Chest)')
+  action('Pickup(Smithy,Sword)') 
+  action('WalkTo(Smithy,Blacksmith.Chest)') 
+  action('LookAt(Smithy,Blacksmith.Chest)') 
   action('Open(Smithy,Blacksmith.Chest)')
   action('Put(Smithy,Sword,Blacksmith.Chest)')
   action('SetExpression(Smithy,surprised)')
