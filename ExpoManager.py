1725819f8a4a7af84467747e65f815eea79d49e6
RileyM117 <98477085+RileyM117@users.noreply.github.com>
2023-04-20 19:37:02 -0500

    Update ExpoManager.py

--- ExpoManager.py
+++ ExpoManager.py
@@ -7,7 +7,7 @@ import os
 import time
 
 print("start ShowMenu()",flush=True)
-
+conversations = []
 def action(command):
     print('start ' + command)
     while(True):
@@ -22,8 +22,8 @@ def action(command):
 
 definite_locations = ['Camp','City','AlchemyShop','Blacksmith','GreatHall','Courtyard','Ruins','Tavern','Library']
 city_char = False
-init_prompt = "These are the characters you can use: " +  ', '.join(char_names[1:-1]) + ". These are the locations you can use: " + ', '.join(definite_locations) + ". Write an unsolved murder set in a medieval/fantasy setting using these characters and locations. Do not include an investigation or an investigator character. The mystery you write is unsolved. "
-os.environ["OPENAI_API_KEY"]="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"
+init_prompt = "These are the characters you can use: " +  ', '.join(char_names[1:16]) + ". These are the locations you can use: " + ', '.join(definite_locations) + ". Write an unsolved murder set in a medieval/fantasy setting using these characters and locations. Do not include an investigation or an investigator character. The mystery you write is unsolved. "
+os.environ["OPENAI_API_KEY"]="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"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.ChatCompletion.create(
    model="gpt-3.5-turbo",
@@ -35,7 +35,7 @@ story = completion.choices[0].message.content
 with open("C:/Users/riley/OneDrive/Desktop/AIOutput.txt", "w") as f:
     response = f.write(story)
 
-os.environ["OPENAI_API_KEY"]="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"
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.ChatCompletion.create(
    model="gpt-3.5-turbo",
@@ -63,7 +63,7 @@ for i in char_list[0:1]:
 
 
 while(True):
-  conversations = []
+  #conversations = []
   i = input()
   if(i == 'input Selected Start'):
    for i in definite_locations[2:9]:
@@ -107,13 +107,12 @@ while(True):
         answer = str(USER_INP)
         answer = answer.replace(',','')
        
-        action("SetDialog(\""+answer+"\")")
+        action("SetDialog("+char_list[0].name+": "+answer+")")
         action("SetLeft(\""+char_list[0].name+"\")")
         action('ShowDialog()')
      
         #
 
-        os.environ["OPENAI_API_KEY"]="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"
         openai.api_key = os.getenv("OPENAI_API_KEY")
         completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -123,24 +122,32 @@ while(True):
         )
         story3 = completion.choices[0].message.content
         story3 = story3.replace('"','')
+        story3 = story3.replace(',','')
+        #conversations.append((char_list[0].name + ":"  + answer, j.name +":" + story3))
         with open("C:/Users/riley/OneDrive/Desktop/AIOutput.txt", "w") as f:
           response = f.write(story)
-        action("SetDialog(\""+story3+"\")")
-        action("SetRight(\""+j.name+"\")")
+        #action("SetDialog(\""+story3+"\"[Leave Chat|Leave Chat])")
+        action("SetDialog("+story3+"[Respond|Respond] [Leave Chat|Leave Chat])")
+        action("SetRight(\""+j.name+"\" )")
         action("ShowDialog()")
-        time.sleep(7)
-        action("HideDialog()")
-        
-        action('EnableInput()')
         action("EnableInput()")
-        conversations.append((char_list[0].name + ":"  + answer, j.name +":" + story3))
-  
-
+        
+        #time.sleep(7)
+        #action("HideDialog()")
+        
+        #action('EnableInput()')
+        #action("EnableInput()")
+     
+        conversations.append("Player: " + answer)
+        conversations.append("NPC: " + story3)
+        #for i in conversations:
+        #   i = i.replace(',','')
+        #   i = i.replace('"','')
 
   if(i == 'input arrived ' + char_list[0].name + ' position Camp.Exit'): 
      action("SetPosition({},City)".format(char_list[0].name))
      if city_char == False:
-        for i in char_list[1:19]:
+        for i in char_list[1:16]:
             action("CreateCharacter({},{})".format(i.name,i.body))
             action("SetEyeColor({},{})".format(i.name,i.eye_color))
             action("SetHairColor({},{})".format(i.name,i.hair_color))
@@ -150,17 +157,62 @@ while(True):
             action("SetPosition({},{})".format(i.name,i.location))
             action('EnableIcon("Talk to", Talk,'+i.name+', "Talk to")')
             city_char = True;
+  if(i == 'input Selected Leave Chat'):
+     #action("EnableInput()")
+     action("HideDialog()")
+  if(i == 'input Selected Respond'):
+      ROOT = tk.Tk()
+      ROOT.withdraw()
+      #ask = ("What do you say?")
+      #action("SetDialog("+ask+")") 
+      #action('ShowDialog()')
+      USER_INP = simpledialog.askstring(title="Test", prompt="What do you Say?:")
+      answer = str(USER_INP)
+      answer = answer.replace(',','')
+      
+      action("SetDialog("+char_list[0].name+": "+answer+")")
+      action("SetLeft(\""+char_list[0].name+"\")")
+      action('ShowDialog()')
      
+      
 
+      openai.api_key = os.getenv("OPENAI_API_KEY")
+      completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+      {"role": "user", "content": "This is the initial prompt that I gave you: " + init_prompt + "This is the murder mystery that you wrote: " + story + " This is the short description that you provided the player: " + story2 + ". Based on this, the player talks to " + j.name + ". They say " + answer + ". Based on: " + story + " What does " + j.name + " say in response to the player in a dialogue format?"}
+          ]
+      )
+      story3 = completion.choices[0].message.content
+      story3 = story3.replace('"','')
+      story3 = story3.replace(',','')
+      #conversations.append((char_list[0].name + ":"  + answer, j.name +":" + story3))
+      with open("C:/Users/riley/OneDrive/Desktop/AIOutput.txt", "w") as f:
+        response = f.write(story)
+      #action("SetDialog(\""+story3+"\"[Leave Chat|Leave Chat])")
+      action("SetDialog("+story3+"[Respond|Respond] [Leave Chat|Leave Chat])")
+      action("SetRight(\""+j.name+"\" )")
+      action("ShowDialog()")
+      action("EnableInput()")
+      
+      #time.sleep(7)
+      #action("HideDialog()")
+      
+      #action('EnableInput()')
+      #action("EnableInput()")
+     
+      conversations.append("Player: " + answer)
+      conversations.append("NPC: " + story3)
   if(i == 'input Key Inventory'):
      action("ShowList({})".format(char_list[0].name))
-   
+
   if(i =='input Close List'):
      action("HideList()")
 
   if(i == 'input Review Conversations BlueBook'):
      action("SetNarration(\""+' '.join(conversations)+"\")")
      action("ShowNarration()")
+ 
   if(i == 'input Close Narration'):
      action("HideNarration()")
 
@@ -217,3 +269,4 @@ while(True):
   if(i == 'input Open Door Tavern.Door'):
      #action("OpenFurniture({},Tavern.Door)".format(char_list[0].name))
      action("SetPosition({},City.RedHouseDoor)".format(char_list[0].name))
+
